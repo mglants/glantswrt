dca2950a87d7327b3a12c2ebc9de15052b0f5f2e
Mark Glants <mark@glants.xyz>
2026-03-05 00:58:25 +0300

    feat: not yet ready for 25.0

--- devices/kn3510/env.sh
+++ devices/kn3510/env.sh
@@ -16,7 +16,7 @@
 set -euo pipefail
 OPENWRT_CHANNEL="release"
 # renovate: datasource=git-tags depName=https://github.com/openwrt/openwrt.git
-OPENWRT_VERSION="v25.12.0"
+OPENWRT_VERSION="v24.10.5"
 
 TARGET="ramips"
 SUBTARGET="mt7621"
